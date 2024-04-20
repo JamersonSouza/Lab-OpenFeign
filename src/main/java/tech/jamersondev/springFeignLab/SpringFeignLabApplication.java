b0c596231a6849e3a990c121c3607468e7cd5d85
jamersonsouza <jamersonsouza@outlook.com>
2024-04-20 11:16:15 -0300

    Lab consumo de api com feign

---
+++ src/main/java/tech/jamersondev/springFeignLab/SpringFeignLabApplication.java
@@ -0,0 +1,13 @@
+package tech.jamersondev.springFeignLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeignLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFeignLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/jamersondev/springFeignLab/SpringFeignLabApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jamersondev.springFeignLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeignLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
